efc083301b9e8bd3259e8374b54c1ac2926c3b52
Ieva Stripkāne <ieva.stripkane.2002@gmail.com>
2020-05-06 08:40:51 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace ColorMixerStudio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.RedBox = new System.Windows.Forms.PictureBox();
+            this.GreenBox = new System.Windows.Forms.PictureBox();
+            this.RedSlider = new System.Windows.Forms.TrackBar();
+            this.GreenSlider = new System.Windows.Forms.TrackBar();
+            this.BlueSlider = new System.Windows.Forms.TrackBar();
+            this.RandomRed = new System.Windows.Forms.Button();
+            this.BlueTimerStart = new System.Windows.Forms.Button();
+            this.RandomGreen = new System.Windows.Forms.Button();
+            this.GreenTimerStart = new System.Windows.Forms.Button();
+            this.RedTimerStart = new System.Windows.Forms.Button();
+            this.RandomBlue = new System.Windows.Forms.Button();
+            this.RandomMix = new System.Windows.Forms.Button();
+            this.ResetColors = new System.Windows.Forms.Button();
+            this.BlueBox = new System.Windows.Forms.PictureBox();
+            this.RedTimer = new System.Windows.Forms.Timer(this.components);
+            this.GreenTimer = new System.Windows.Forms.Timer(this.components);
+            this.BlueTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.RedBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GreenBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RedSlider)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GreenSlider)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BlueSlider)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BlueBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // RedBox
+            // 
+            this.RedBox.Location = new System.Drawing.Point(67, 45);
+            this.RedBox.Name = "RedBox";
+            this.RedBox.Size = new System.Drawing.Size(200, 200);
+            this.RedBox.TabIndex = 0;
+            this.RedBox.TabStop = false;
+            // 
+            // GreenBox
+            // 
+            this.GreenBox.Location = new System.Drawing.Point(297, 45);
+            this.GreenBox.Name = "GreenBox";
+            this.GreenBox.Size = new System.Drawing.Size(200, 200);
+            this.GreenBox.TabIndex = 15;
+            this.GreenBox.TabStop = false;
+            // 
+            // RedSlider
+            // 
+            this.RedSlider.Location = new System.Drawing.Point(67, 251);
+            this.RedSlider.Maximum = 255;
+            this.RedSlider.Name = "RedSlider";
+            this.RedSlider.Size = new System.Drawing.Size(200, 56);
+            this.RedSlider.TabIndex = 3;
+            this.RedSlider.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.RedSlider.Scroll += new System.EventHandler(this.RedSlider_Scroll);
+            // 
+            // GreenSlider
+            // 
+            this.GreenSlider.Location = new System.Drawing.Point(297, 251);
+            this.GreenSlider.Maximum = 255;
+            this.GreenSlider.Name = "GreenSlider";
+            this.GreenSlider.Size = new System.Drawing.Size(200, 56);
+            this.GreenSlider.TabIndex = 4;
+            this.GreenSlider.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.GreenSlider.Scroll += new System.EventHandler(this.GreenSlider_Scroll);
+            // 
+            // BlueSlider
+            // 
+            this.BlueSlider.Location = new System.Drawing.Point(519, 251);
+            this.BlueSlider.Maximum = 255;
+            this.BlueSlider.Name = "BlueSlider";
+            this.BlueSlider.Size = new System.Drawing.Size(200, 56);
+            this.BlueSlider.TabIndex = 5;
+            this.BlueSlider.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.BlueSlider.Scroll += new System.EventHandler(this.BlueSlider_Scroll);
+            // 
+            // RandomRed
+            // 
+            this.RandomRed.Location = new System.Drawing.Point(67, 304);
+            this.RandomRed.Name = "RandomRed";
+            this.RandomRed.Size = new System.Drawing.Size(200, 65);
+            this.RandomRed.TabIndex = 6;
+            this.RandomRed.Text = "Random Red";
+            this.RandomRed.UseVisualStyleBackColor = true;
+            this.RandomRed.Click += new System.EventHandler(this.RandomRed_Click);
+            // 
+            // BlueTimerStart
+            // 
+            this.BlueTimerStart.Location = new System.Drawing.Point(519, 385);
+            this.BlueTimerStart.Name = "BlueTimerStart";
+            this.BlueTimerStart.Size = new System.Drawing.Size(200, 65);
+            this.BlueTimerStart.TabIndex = 7;
+            this.BlueTimerStart.Text = "Blue Party";
+            this.BlueTimerStart.UseVisualStyleBackColor = true;
+            this.BlueTimerStart.Click += new System.EventHandler(this.BlueTimerStart_Click);
+            // 
+            // RandomGreen
+            // 
+            this.RandomGreen.Location = new System.Drawing.Point(297, 304);
+            this.RandomGreen.Name = "RandomGreen";
+            this.RandomGreen.Size = new System.Drawing.Size(200, 65);
+            this.RandomGreen.TabIndex = 8;
+            this.RandomGreen.Text = "Random Green";
+            this.RandomGreen.UseVisualStyleBackColor = true;
+            this.RandomGreen.Click += new System.EventHandler(this.RandomGreen_Click);
+            // 
+            // GreenTimerStart
+            // 
+            this.GreenTimerStart.Location = new System.Drawing.Point(297, 385);
+            this.GreenTimerStart.Name = "GreenTimerStart";
+            this.GreenTimerStart.Size = new System.Drawing.Size(200, 65);
+            this.GreenTimerStart.TabIndex = 9;
+            this.GreenTimerStart.Text = "Green Party";
+            this.GreenTimerStart.UseVisualStyleBackColor = true;
+            this.GreenTimerStart.Click += new System.EventHandler(this.GreenTimerStart_Click);
+            // 
+            // RedTimerStart
+            // 
+            this.RedTimerStart.Location = new System.Drawing.Point(67, 385);
+            this.RedTimerStart.Name = "RedTimerStart";
+            this.RedTimerStart.Size = new System.Drawing.Size(200, 65);
+            this.RedTimerStart.TabIndex = 10;
+            this.RedTimerStart.Text = "Red Party";
+            this.RedTimerStart.UseVisualStyleBackColor = true;
+            this.RedTimerStart.Click += new System.EventHandler(this.RedTimerStart_Click);
+            // 
+            // RandomBlue
+            // 
+            this.RandomBlue.Location = new System.Drawing.Point(519, 304);
+            this.RandomBlue.Name = "RandomBlue";
+            this.RandomBlue.Size = new System.Drawing.Size(200, 65);
+            this.RandomBlue.TabIndex = 11;
+            this.RandomBlue.Text = "Random Blue";
+            this.RandomBlue.UseVisualStyleBackColor = true;
+            this.RandomBlue.Click += new System.EventHandler(this.RandomBlue_Click);
+            // 
+            // RandomMix
+            // 
+            this.RandomMix.Location = new System.Drawing.Point(67, 466);
+            this.RandomMix.Name = "RandomMix";
+            this.RandomMix.Size = new System.Drawing.Size(652, 65);
+            this.RandomMix.TabIndex = 12;
+            this.RandomMix.Text = "Random Mix";
+            this.RandomMix.UseVisualStyleBackColor = true;
+            this.RandomMix.Click += new System.EventHandler(this.RandomMix_Click);
+            // 
+            // ResetColors
+            // 
+            this.ResetColors.Location = new System.Drawing.Point(67, 547);
+            this.ResetColors.Name = "ResetColors";
+            this.ResetColors.Size = new System.Drawing.Size(652, 65);
+            this.ResetColors.TabIndex = 13;
+            this.ResetColors.Text = "Reset Colors";
+            this.ResetColors.UseVisualStyleBackColor = true;
+            this.ResetColors.Click += new System.EventHandler(this.ResetColors_Click);
+            // 
+            // BlueBox
+            // 
+            this.BlueBox.Location = new System.Drawing.Point(519, 45);
+            this.BlueBox.Name = "BlueBox";
+            this.BlueBox.Size = new System.Drawing.Size(200, 200);
+            this.BlueBox.TabIndex = 16;
+            this.BlueBox.TabStop = false;
+            // 
+            // RedTimer
+            // 
+            this.RedTimer.Tick += new System.EventHandler(this.RedTimer_Tick);
+            // 
+            // GreenTimer
+            // 
+            this.GreenTimer.Tick += new System.EventHandler(this.GreenTimer_Tick);
+            // 
+            // BlueTimer
+            // 
+            this.BlueTimer.Tick += new System.EventHandler(this.BlueTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 665);
+            this.Controls.Add(this.BlueBox);
+            this.Controls.Add(this.ResetColors);
+            this.Controls.Add(this.RandomMix);
+            this.Controls.Add(this.RandomBlue);
+            this.Controls.Add(this.RedTimerStart);
+            this.Controls.Add(this.GreenTimerStart);
+            this.Controls.Add(this.RandomGreen);
+            this.Controls.Add(this.BlueTimerStart);
+            this.Controls.Add(this.RandomRed);
+            this.Controls.Add(this.BlueSlider);
+            this.Controls.Add(this.GreenSlider);
+            this.Controls.Add(this.RedSlider);
+            this.Controls.Add(this.GreenBox);
+            this.Controls.Add(this.RedBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.RedBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GreenBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RedSlider)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GreenSlider)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BlueSlider)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BlueBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox RedBox;
+        private System.Windows.Forms.PictureBox GreenBox;
+        private System.Windows.Forms.TrackBar RedSlider;
+        private System.Windows.Forms.TrackBar GreenSlider;
+        private System.Windows.Forms.TrackBar BlueSlider;
+        private System.Windows.Forms.Button RandomRed;
+        private System.Windows.Forms.Button BlueTimerStart;
+        private System.Windows.Forms.Button RandomGreen;
+        private System.Windows.Forms.Button GreenTimerStart;
+        private System.Windows.Forms.Button RedTimerStart;
+        private System.Windows.Forms.Button RandomBlue;
+        private System.Windows.Forms.Button RandomMix;
+        private System.Windows.Forms.Button ResetColors;
+        private System.Windows.Forms.PictureBox BlueBox;
+        private System.Windows.Forms.Timer RedTimer;
+        private System.Windows.Forms.Timer GreenTimer;
+        private System.Windows.Forms.Timer BlueTimer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ColorMixerStudio
+{
+    public partial class Form1 : Form
+    {
+        int red = 0;
+        int green = 0;
+        int blue = 0;
+        int redStep = 1;
+        int greenStep = 1;
+        int blueStep = 1;
+
+        Random rand = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void RandomRed_Click(object sender, EventArgs e)
+        {
+            red = rand.Next(0, 256);
+            RedSlider.Value = red;
+            RedBox.BackColor = Color.FromArgb(red, 0, 0);
+            this.BackColor = Color.FromArgb(red, 0, 0);
+        }
+
+        private void RandomGreen_Click(object sender, EventArgs e)
+        {
+            green = rand.Next(0, 256);
+            GreenSlider.Value = green;
+            GreenBox.BackColor = Color.FromArgb(0, green, 0);
+            this.BackColor = Color.FromArgb(0, green, 0);
+        }
+
+        private void RandomBlue_Click(object sender, EventArgs e)
+        {
+            blue = rand.Next(0, 256);
+            BlueSlider.Value = blue;
+            BlueBox.BackColor = Color.FromArgb(0, 0, blue);
+            this.BackColor = Color.FromArgb(0, 0, blue);
+        }
+
+        private void RandomMix_Click(object sender, EventArgs e)
+        {
+            red = rand.Next(0, 256);
+            green = rand.Next(0, 256);
+            blue = rand.Next(0, 256);
+          
+            RedSlider.Value = red;
+            GreenSlider.Value = green;
+            BlueSlider.Value = blue;
+
+            RedBox.BackColor = Color.FromArgb(red, 0, 0);
+            GreenBox.BackColor = Color.FromArgb(0, green, 0);
+            BlueBox.BackColor = Color.FromArgb(0, 0, blue);
+            this.BackColor = Color.FromArgb(red, green, blue);
+        }
+
+        private void RedSlider_Scroll(object sender, EventArgs e)
+        {
+            UpdateRed();
+        }
+
+        private void GreenSlider_Scroll(object sender, EventArgs e)
+        {
+            UpdateGreen();
+        }
+
+        private void BlueSlider_Scroll(object sender, EventArgs e)
+        {
+            UpdateBlue();
+        }
+
+        private void ResetColors_Click(object sender, EventArgs e)
+        {
+            red = 0;
+            green = 0;
+            blue = 0;
+
+            RedSlider.Value = red;
+            GreenSlider.Value = green;
+            BlueSlider.Value = blue;
+
+            RedBox.BackColor = Color.FromArgb(red, 0, 0);
+            GreenBox.BackColor = Color.FromArgb(0, green, 0);
+            BlueBox.BackColor = Color.FromArgb(0, 0, blue);
+            this.BackColor = Color.FromArgb(red, green, blue);
+        }
+
+        private void RedTimer_Tick(object sender, EventArgs e)
+        {
+            RedSlider.Value += redStep;
+            if (RedSlider.Value == RedSlider.Maximum || RedSlider.Value == RedSlider.Minimum)
+            {
+                redStep *= -1; //redStep * (-1);
+            }
+            UpdateRed();
+        }
+
+        private void GreenTimer_Tick(object sender, EventArgs e)
+        {
+            if (GreenSlider.Value == GreenSlider.Maximum)
+            {
+                greenStep = -1;
+            }
+            else if (GreenSlider.Value == GreenSlider.Minimum)
+            {
+                greenStep = 1;
+            }
+            GreenSlider.Value += greenStep;
+            UpdateGreen();
+        }
+
+        private void BlueTimer_Tick(object sender, EventArgs e)
+        {  
+            if (BlueSlider.Value == BlueSlider.Maximum)
+            {
+                blueStep = -1;
+            }
+            else if (BlueSlider.Value == BlueSlider.Minimum)
+            {
+                blueStep = 1;
+            }
+            BlueSlider.Value += blueStep;
+            UpdateBlue();
+        }
+
+        private void RedTimerStart_Click(object sender, EventArgs e)
+        {
+            RedTimer.Start();
+        }
+
+        private void GreenTimerStart_Click(object sender, EventArgs e)
+        {
+            GreenTimer.Start();
+        }
+
+        private void BlueTimerStart_Click(object sender, EventArgs e)
+        {
+            BlueTimer.Start();
+        }
+
+        private void UpdateRed()
+        {
+            red = RedSlider.Value;
+            RedBox.BackColor = Color.FromArgb(red, 0, 0);
+            this.BackColor = Color.FromArgb(red, green, blue);
+        }
+
+        private void UpdateGreen()
+        {
+            green = GreenSlider.Value;
+            GreenBox.BackColor = Color.FromArgb(0, green, 0);
+            this.BackColor = Color.FromArgb(red, green, blue);
+        }
+
+        private void UpdateBlue()
+        {
+            blue = BlueSlider.Value;
+            BlueBox.BackColor = Color.FromArgb(0, 0, blue);
+            this.BackColor = Color.FromArgb(red, green, blue);
+        }
+    }
+
+
+        
+}
